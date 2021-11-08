e3f714e984e4d779a8c825df49bdaf7f344ff253
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-08 17:39:51 -0500

    working on it

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
@@ -29,7 +32,12 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
+    <Route exact path='/'>
+      <MovieList movies={movieList} />
+    </Route>
+    <Route path='/movies/:id'>
+      <Movie />
+    </Route>
       <div>Replace this Div with your Routes</div>
     </div>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  let { id } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -19,7 +20,7 @@ export default function Movie(props) {
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
